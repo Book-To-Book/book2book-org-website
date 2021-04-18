6d33cf05797437ec83550d63ce5c0620230fd2ef
gburatti <gburatti.rc@gmail.com>
2021-04-18 12:45:28 +0200

    correzione img su mobile, slider da destra su desktop, piccole correzioni

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -1,6 +1,7 @@
 import doubleMockup from '../assets/img/mockups/double-mockup.png';
 import iphonesMockup from '../assets/img/mockups/Iphone_mockup_scaled.png';
 import whiteMockup from '../assets/img/mockups/homepage-mockup-white-phone.png';
+import emailIcon from '../assets/img/icon_email_white.png';
 
 import './Landing.css';
 
@@ -23,21 +24,23 @@ const Landing = () => (
 
     <header className="App-header">
 
-      <div className="col-6">
+      <div className="col-6 centered-flex">
         <img src={iphonesMockup} alt="Book2Book" style={{ width: '100%' }} />
       </div>
 
-      <div className="col-6 hidden-overflow centered-flex">
-        <div className="slide-in hidden-overflow slider-inverse">
+      <div className="col-6 centered-flex">
+        <div className="slide-in slider">
           <h2 className="superbold">Book2Book</h2>
-          <h5 style={{ textTransform: 'uppercase' }}>
-            La prima <b>biblioteca di prossimità</b> in Italia
-          </h5>
+          <h4 className="overflow-margin" style={{ textTransform: 'uppercase'}}>
+            La prima biblioteca di prossimità in Italia
+          </h4>
           <small>
             Stay tuned!
           </small>
           <br/>
-          <a id="email" href="mailto:info@book2book.org">info@book2book.org</a>
+          <a className="email" href="mailto:info@book2book.org">
+          <img src={emailIcon} alt="BandiPubblici"/> info@book2book.org
+        </a>
         </div>
       </div>
 
@@ -60,7 +63,10 @@ const Landing = () => (
         </div>
 
         <div className="slide-in hidden-overflow slider-inverse">
-          <img src={whiteMockup} alt="BandiPubblici" style={{ width: '200px' }} />
+          <img src={iphonesMockup} alt="BandiPubblici" style={{ maxWidth: '90%' }} />
+          <a className="email" href="mailto:info@book2book.org">
+            <img src={emailIcon} alt="BandiPubblici"/> info@book2book.org
+          </a>
         </div>
       </div>
 
