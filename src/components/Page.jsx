609c1aed6ef754920ea16bd616d9940c45431224
Giuliano Buratti <gburatti.rc@gmail.com>
2023-02-22 12:47:43 +0100

    layout fix

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -7,7 +7,7 @@ const Page = ({children}) => {
   const bgColor = BG_COLOR_PAGES.includes(pathname);
 
   return (
-    <div className={`w-full min-h-full ${bgColor ? "bg-gradient-to-b from-pink to-orange" : "bg-white"}`}>
+    <div className={`w-full min-h-full overflow-hidden ${bgColor ? "bg-gradient-to-b from-pink to-orange" : "bg-white"}`}>
       <div className="w-full max-w-5xl sm:px-20 max-sm:px-10 pt-40 sm:pb-80 max-sm:pb-96 mx-auto">
         {children}
       </div>
